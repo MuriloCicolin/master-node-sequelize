2995fb1001cf10843694ee50d54189e53a6fad80
MuriloCicolin <mu_cicolin@hotmail.com>
2020-01-09 14:34:35 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mariadb',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '1234',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
